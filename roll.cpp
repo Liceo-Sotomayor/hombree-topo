18880098638fd942844e9a4b02dd561bba270494
LucaMutis <lmutisg@gmail.com>
2017-06-24 11:16:26 -0400

    juego de rol

--- roll.cpp
+++ roll.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
-#include <string>
+#include <cmath>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 int main() {
-    int a, b;
-    bool esMayor;
-    cout << " ingrse un numero " << endl;
-    cin >> a;
-    cout << " ingrese otro numero " << endl;
-    cin >> b;
-    esMayor = a > b;
-    if (esMayor) {
-    cout << " el numero " << a << " es mayor que " << b << endl;
+    srand (time(NULL));
+    int arma, escudo, dano;
+    
+    arma = rand()%6+1;
+    arma += rand()%6+1;
+    arma += 3;
+
+    escudo = rand()%4+1;
+    escudo += rand()%4+1;
+    escudo += 1;
+
+    dano = arma - escudo;
+ 
+    if (dano > 0) {
+        cout << " El dano es " << dano << endl;
+    } else {
+        cout << " No sufrio dano " << endl;
     }
 }
